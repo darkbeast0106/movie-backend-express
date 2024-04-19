f34f5dd3141bcb9939dfb35773b4b077349be89b
Budaházi Bence <budahazi.bence@petrik.hu>
2024-04-19 09:20:49 +0200

    fix: Add cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
 import movieRoutes from "./routes/movies.js";
+import cors from "cors";
 
 const app = express();
 const port = process.env.APP_PORT || 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/movies", movieRoutes);
